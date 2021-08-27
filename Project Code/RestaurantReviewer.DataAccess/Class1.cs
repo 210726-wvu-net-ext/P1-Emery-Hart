15cd2867720cd42ff5c7add925db88e19d481920
Ehart <Ehart3@masonlive.gmu.edu>
2021-08-27 15:20:24 -0400

    More Framing

---
+++ Project Code/RestaurantReviewer.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RestaurantReviewer.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ Project Code/RestaurantReviewer.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RestaurantReviewer.Domain
+{
+    public class Class1
+    {
+    }
+}
