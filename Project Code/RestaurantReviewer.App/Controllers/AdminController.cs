7ac51a1210281b74905807b04a4337699c99577d
Ehart <Ehart3@masonlive.gmu.edu>
2021-09-07 19:45:29 -0400

    Basic Admin functions added

---
+++ Project Code/RestaurantReviewer.App/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Serilog;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestaurantReviewer.Domain;
+
+namespace RestaurantReviewer.App.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IRepository _repo;
+
+        public AdminController(IRepository repo)
+        {
+            _repo = repo;
+        }
+        public IActionResult Index()
+        {
+            Log.Information("Admin mode enabled");
+            return View(_repo.GetUsers());
+        }
+    }
+}
